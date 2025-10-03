5667d919a4cf649208f37d1c67f5c993cc4a5108
kanhaiya41 <161762630+kanhaiya41@users.noreply.github.com>
2025-10-03 14:40:20 +0530

    mail service

--- backend/index.js
+++ backend/index.js
@@ -88,12 +88,16 @@ app.post('/send-mail', async (req, res) => {
     `;
 
     const transporter = nodemailer.createTransport({
-      service: 'gmail',
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
+      requireTLS: true,
       auth: {
         user: process.env.USER_EMAIL,
         pass: process.env.EMAIL_PASS,
-      },
+      }
     });
+
 
     const mailOptions = {
       from: `"ICA Website Contact" <${process.env.USER_EMAIL}>`,
